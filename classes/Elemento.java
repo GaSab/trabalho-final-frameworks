89f83a7355a0361edb0d82a6e2969dd12d124825
gabriel araujo saboya <gabriel_a_saboya@hotmail.com>
2018-06-16 16:24:05 -0300

    iniciando

---
+++ classes/Elemento.java
@@ -0,0 +1,19 @@
+
+public class Elemento {
+	protected int id;
+	
+	public Elemento() {
+	}
+	
+	public Elemento (int i) {
+		id = i;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
