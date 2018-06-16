dc70d49b83aeb7e89b1aaa72b2d9a03eefc192be
gabriel araujo saboya <gabriel_a_saboya@hotmail.com>
2018-06-16 16:32:05 -0300

    revisitando william

---
+++ classes/Produto.java
@@ -0,0 +1,38 @@
+
+public class Produto extends Elemento {
+
+	protected String nome, descricao, preco;
+
+	public Produto() {
+		super();
+	}
+
+	public Produto(int i) {
+		super(i);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String str) {
+		nome = str;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String str) {
+		descricao = str;
+	}
+
+	public String getPreco() {
+		return preco;
+	}
+
+	public void setPreco(String str) {
+		preco = str;
+	}
+
+}
