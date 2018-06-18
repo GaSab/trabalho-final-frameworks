cd33e30cffe1b18954ba201199b8b0ad95ee4a10
Wrilliam <william.eti@alu.ufc.br>
2018-06-18 20:04:27 -0300

    Implementando os métodos do CRUD

--- classes/Registro.java
+++ classes/Registro.java
@@ -12,8 +12,28 @@ public class Registro {
 		reg.put(3, new Produto("Mouse óptico", "made by Multilaser", 18.0));
 	}
 	
-	public void adicionar() {}
+	public void adicionar(Integer i, Elemento e) {
+//		reg.putIfAbsent(i, e);
+		if(reg.containsKey(i) == false)
+			reg.put(i, e);
+	} // adicionar um elemento à cadeia, se a chave não tiver sido usada
 	
-	public void excluir() {}
+	public void excluir(Integer i) {
+		reg.remove(i);
+	} // exclui o elemento cuja chave corresponde ao inteiro i
+
+	public void atualizar(Integer i, Elemento e) {
+	    if(reg.containsKey(i) == true) {
+	    	// reg.put(i, e);
+	        reg.replace(i, e);
+	    } // atualiza o elemento correspondente à chave
+	}
+
+	public Elemento buscar(Integer i) {
+	    if(reg.containsKey(i) == true) {
+	        return reg.get(i);
+	    }
+	    return null;
+	} // retorna o elemento correspondente à chave
 
 }
