73cb2fb3f6679caa30df082a7e1203d19c959810
Ribeiro <7347861+wrilliam@users.noreply.github.com>
2018-06-19 22:06:49 -0300

    Atualizando

--- classes/Usuario.java
+++ classes/Usuario.java
@@ -1,21 +1,36 @@
 
 abstract public class Usuario extends Elemento {
 
-	private protected String nome, login, senha;
+	protected String login, senha;
 
 	public Usuario() {
 		super();
 	}
 
-	public Usuario(int i) {
-		super(i);
+	public Usuario(String str) {
+		super(str);
 	}
 
 	public Usuario(String str1, String str2, String str3) {
-		super();
-		nome = str1;
+		super(str1);
 		login = str2;
 		senha = str3;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String str) {
+		login = str;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String str) {
+		senha = str;
 	}
 
 }
